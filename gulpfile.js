c23992a0c25da11ff52464854c622835ce423b53
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-03-03 09:53:42 +1300

    Configure gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,27 @@
 /**
  * Created by keilc on 3/03/2017.
  */
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+// gulp-typescript
+gulp.task('default', function () {
+  return gulp.src(['src/**/*.ts', '!node_modules/**'])
+    .pipe(ts({
+      noImplicitAny: false,
+    }))
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
+});
+
+// gulp-jshint
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js', '!node_modules/**'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch(['src/**/*.ts', '!node_modules/**'], ['default']);
+});
